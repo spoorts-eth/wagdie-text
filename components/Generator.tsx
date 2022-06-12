057ffeb069416f0093bc7e94a194fdd1f050de50
spoorts <spaawts@gmail.com>
2022-06-12 19:17:48 +0100

    update deps

---
+++ components/Generator.tsx
@@ -0,0 +1,3 @@
+export default function Generator() {
+    return null;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Generator from '../components/Generator'
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
 
       <main>
         <h1>WAGDIE Text Generator</h1>
+        <Generator />
       </main>
     </div>
   )
