2819c38e1c19350db71d854cebaec9974c5a5923
spoorts <spaawts@gmail.com>
2022-06-12 19:11:03 +0100

    Initial commit.

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WAGDIE Text Generator</title>
+        <meta name="description" content="Generate WAGDIE style text" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+
+      <main>
+        <h1>WAGDIE Text Generator</h1>
+      </main>
+    </div>
+  )
+}
