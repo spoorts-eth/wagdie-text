b68742b22661143f3f5690d1bbb061f6a60b8c54
spoorts <spaawts@gmail.com>
2022-06-13 13:23:44 +0100

    add credits

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,14 +13,46 @@ export default function Home() {
       </Head>
 
       <main className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.logo}>
-            <Image src="/wagdie.png" alt="WAGDIE" width={256} height={256} />
+        <div className={styles.containerInner}>
+          <div className={styles.header}>
+            <div className={styles.logo}>
+              <Image src="/wagdie.png" alt="WAGDIE" width={256} height={256} />
+            </div>
+            <h1 className={styles.title}>WAGDIE Text Generator</h1>
           </div>
-          <h1 className={styles.title}>WAGDIE Text Generator</h1>
-        </div>
 
-        <Generator />
+          <Generator />
+          <div className={styles.footer}>
+            <a
+              href="https://github.com/spoorts-eth/wagdie-text"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/github.svg"
+                alt="GitHub"
+                width={32}
+                height={32}
+                color="white"
+              />
+            </a>
+          </div>
+          <a
+            href="https://twitter.com/spaawts"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className={styles.credits}>
+              <Image
+                src="/spoorts.png"
+                alt="spoorts.eth"
+                width={64}
+                height={64}
+              />
+              <span>Created by Spoorts.eth</span>
+            </div>
+          </a>
+        </div>
       </main>
     </>
   )
